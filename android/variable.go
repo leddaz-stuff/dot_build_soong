LineageOS/android_build_soong

soong: Include Qualcomm product variables
Change-Id: Ic514f609257ca5d49174ae996e8adfa9dfb7b540

android/variable.go
@@ -90,6 +90,7 @@ type variableProperties struct {
 
 		// include Lineage variables
 		Lineage android.Product_variables
+		Qualcomm android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -158,6 +159,7 @@ type productVariables struct {
 
 	// include Lineage variables
 	Lineage android.ProductVariables
+	Qualcomm android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {

leddaz-stuff/dot_build_soong

Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>
Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

android/variable.go
@@ -99,6 +99,7 @@ type variableProperties struct {
 
 		// include Aosp variables
 		Aosp android_aosp.Product_variables
+		Qualcomm android_aosp.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -175,6 +176,7 @@ type productVariables struct {
 
 	// include Aosp variables
 	Aosp android_aosp.ProductVariables
+	Qualcomm android_aosp.ProductVariables
 }
 
 func boolPtr(v bool) *bool {