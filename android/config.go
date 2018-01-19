LineageOS/android_build_soong

soong: Add function to return camera parameters library name
Change-Id: I3113ea6813b82c3f6b250205080030ff9c2255ec

android/config.go
@@ -500,6 +500,10 @@ func (c *deviceConfig) BtConfigIncludeDir() string {
 	return String(c.config.ProductVariables.BtConfigIncludeDir)
 }
 
+func (c *deviceConfig) SpecificCameraParametersLibrary() string {
+	return String(c.config.ProductVariables.Lineage.Specific_camera_parameter_library)
+}
+
 func (c *deviceConfig) NativeCoverageEnabled() bool {
 	return Bool(c.config.ProductVariables.NativeCoverage)
 }

leddaz-stuff/dot_build_soong

Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

android/config.go
@@ -523,6 +523,10 @@ func (c *deviceConfig) DeviceKernelHeaderDirs() []string {
 	return c.config.ProductVariables.DeviceKernelHeaders
 }
 
+func (c *deviceConfig) SpecificCameraParametersLibrary() string {
+	return String(c.config.ProductVariables.Aosp.Specific_camera_parameter_library)
+}
+
 func (c *deviceConfig) NativeCoverageEnabled() bool {
 	return Bool(c.config.ProductVariables.NativeCoverage)
 }